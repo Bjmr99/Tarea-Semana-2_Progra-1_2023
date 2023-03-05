32eb4a6ffb0b3e65d7cbb4d38a53027d531debe1
Bryan <Bryan@DESKTOP-CV7TRBE>
2023-03-05 00:18:30 -0600

    Agregar archivos de proyecto.

---
+++ Tarea Semana 2_Progra 1_2023/Program.cs
@@ -0,0 +1,31 @@
+﻿//EJERCICIO 1
+Console.WriteLine("Ingrese su Edad: ");
+int edad = int.Parse(Console.ReadLine());
+Console.WriteLine("No aparentas " + edad + "años");
+Console.ReadKey();
+
+//EJERCICIO 2
+byte num1, num2;
+ushort resultado;
+Console.WriteLine("Ingrese el primer numero");
+num1 = Byte.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese el segundo numero");
+num2 = Byte.Parse(Console.ReadLine());
+resultado = Convert.ToUInt16(num1 * num2);
+Console.WriteLine("El resultado es: " + resultado);
+
+//EJERCICIO 3
+long num1, num2;
+long suma, resta, producto;
+Console.WriteLine("\t\tINGRESE NUMEROS EN FORMATO LARGO, EJ: 12500000255874625 \n");
+Console.WriteLine("Ingrese el primer numero ");
+num1 = long.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese el segundo numero");
+num2 = long.Parse(Console.ReadLine());
+suma = num1 + num2;
+resta = num1 - num2;
+producto = num1 * num2;
+Console.WriteLine("El resultado de la SUMA: " + suma);
+Console.WriteLine("El resultado de la RESTA: " + resta);
+Console.WriteLine("El resultado de su PRODUCTO: " + producto);
+
